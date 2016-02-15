a51d454463703ffa662ca4aa661ea7b3673a2a39
Denis <helllion91@gmail.com>
2016-02-15 19:48:36 +0200

    create repository

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+function __autoload($class_name) {
+    $file = './library/' . $class_name . '.php';
+    if (file_exists($file)) {
+        include_once $file;
+    }
+}
+
+function useObject(A $obj) {
+    $obj->test();
+}
+//useObject(new A());
+useObject(new B());
---
+++ library/A.php
@@ -0,0 +1,6 @@
+<?php
+class A {
+    public function test() {
+        echo 'hi i am a test function<br/>';
+    }
+}
